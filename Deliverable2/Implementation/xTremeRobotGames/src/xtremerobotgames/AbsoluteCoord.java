862ffadd65113d7023ac6662122a82c2d776a1e4
Jeroen Peters <jeroentjuh_peters@hotmail.com>
2012-03-13 15:10:28 +0100

    Coords

---
+++ Deliverable2/Implementation/xTremeRobotGames/src/xtremerobotgames/AbsoluteCoord.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package xtremerobotgames;
+
+/**
+ *
+ * @author s102231
+ */
+public class AbsoluteCoord {
+
+    public int x;
+    public int y;
+
+    AbsoluteCoord(int x1, int y1){
+        x = x1;
+        y = y1;
+    }
+
+}
