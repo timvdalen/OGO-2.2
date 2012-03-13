c2cb0f20bc5fda8ac7c1eacc557f661582492a91
Tim van Dalen <Tim@timvdalen.nl>
2012-03-13 22:42:30 +0100

    javascriptBindings toegevoegd

---
+++ Deliverable2/Implementation/viewers/javascriptBindings.php
@@ -0,0 +1,32 @@
+<?php
+	//Read input
+	$input = array();
+	$line = readline("");
+	while($line != "EOF"){
+		$input[] = $line;
+		$line = readline("");
+	}
+
+	//Process afterwards
+	/*
+	I could do it while reading,
+	but this will also work with
+	the ImageMagick version.
+	*/
+	$allframes = array();
+	foreach($input as $frame){
+		$object = json_decode($frame);
+		$allframes[] = $object;
+	}
+	$stringversion = json_encode($allframes);
+?>
+<html>
+	<script type="text/javascript">
+		var jsonstring = "<?=$stringversion?>";
+		var frames = JSON.parse(jsonstring);
+	</script>
+	<!-- Hier de code om de frames te handlen includen -->
+	<div id="wrapper">
+		<canvas id="draw"></canvas>
+	</div>
+</html>
