b78fbf76502b044012bb9bed57c6d9ce88d03a1c
Tim van Dalen <Tim@timvdalen.nl>
2012-03-13 23:34:30 +0100

    Grid anders getekend.

--- Deliverable2/Implementation/viewers/phpDrawFrames.php
+++ Deliverable2/Implementation/viewers/phpDrawFrames.php
@@ -13,14 +13,18 @@
 		$snapshot = $object->snapshot;
 		$frameid = $object->framecount;
 		$im = new Imagick();
-		$im->newImage(330, 330, new ImagickPixel('white'));
+		$im->newImage(321, 321, new ImagickPixel('white'));
 		$draw = new ImagickDraw();
-		$draw->setStrokeColor( new ImagickPixel( 'black' ) );
+		$draw->setStrokeColor(new ImagickPixel('black'));
 		$rowid = 0;
 		foreach($snapshot as $row){
 			$cellid = 0;
 			foreach($row as $cell){
-				$draw->line($cellid * 100 + $cellid * 10, $rowid * 100 + $rowid * 10 , $cellid * 100 + 100 + $cellid * 10, $rowid * 100 + 100 + $rowid * 10);
+				$draw->line($cellid * 100 + $cellid * 10, $rowid * 100 + $rowid * 10 , $cellid * 100 + 100 + $cellid * 10, $rowid * 100 + $rowid * 10);
+				$draw->line($cellid * 100 + $cellid * 10, $rowid * 100 + 100 + $rowid * 10 , $cellid * 100 + 100 + $cellid * 10, $rowid * 100 + 100 + $rowid * 10);
+				$draw->line($cellid * 100 + $cellid * 10, $rowid * 100 + $rowid * 10 , $cellid * 100 + $cellid * 10, $rowid * 100 + 100 + $rowid * 10 );
+				$draw->line($cellid * 100 + 100 + $cellid * 10, $rowid * 100 + $rowid * 10 ,  $cellid * 100 + 100 + $cellid * 10, $rowid * 100 + 100 + $rowid * 10 );
+				$im->annotateImage($draw, $cellid * 100 + $cellid * 10 + 35,  $rowid * 100 + $rowid * 10 + 35, 45, "r: $rowid, c: $cellid");
 				$cellid++;
 			}
 			$rowid++;
